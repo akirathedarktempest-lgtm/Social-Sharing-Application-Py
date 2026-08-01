fbec0ab572ade0e0f904ec582a80267224233468
Yu-zu-i chan! <akirathedarktempest@gmail.com>
2026-08-01 12:44:30 +0530

    Implement user signup functionality with SQLite
    Added a new function to handle user signup and store data in a SQLite database.

--- API/Opinions-API.py
+++ API/Opinions-API.py
@@ -19,11 +19,37 @@ def lookForsignup(email:str):
             pass
     return False
 
+def postingEmail(email,password,username,name):
+    try:
+        connect=sqlite3.connect("signup.db")
+        cursor=connect.cursor()
+        cursor.execute("""CREATE TABLE IF NOT EXISTS sign_up(
+                       email text,
+                       password text,
+                       username text,
+                       name text)""")
+        cursor.execute("INSERT INTO sign_up VALUES (?,?,?,?)",[email,password,username,name])
+        connect.commit()
+        return "Done"
+    except Exception as e:
+        return e
+
 app=FastAPI()
+
+class SigningUp(BaseModel):
+    email:str
+    password:str
+    username:str
+    name:str
 
 @app.get("/get/bool/{email}")
 def get_email_bool(email:str):
     statement=lookForsignup(email)
     return {"Result":f"{statement}"}
+
+@app.post("/post/signup/account")
+def post_email(data:SigningUp):
+    information=postingEmail(data.email,data.password,data.username,data.name)
+    return {"Result":information}
 
 #soon...it will be completed soon
