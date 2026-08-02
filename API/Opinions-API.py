122e9cec46f846206b88d7e2d82a2c7c1febb7e8
Yu-zu-i chan! <akirathedarktempest@gmail.com>
2026-08-02 15:08:10 +0530

    Implement token-based validation for signup
    Added token generation and validation for signup process.

--- API/Opinions-API.py
+++ API/Opinions-API.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import sqlite3
+import tokenGenerator
+
+token=tokenGenerator.generate()
 
 def lookForsignup(email:str):
     connect=sqlite3.connect("signup.db")
@@ -41,15 +44,30 @@ class SigningUp(BaseModel):
     password:str
     username:str
     name:str
+    TOKEN:str
 
 @app.get("/get/bool/{email}")
 def get_email_bool(email:str):
     statement=lookForsignup(email)
-    return {"Result":f"{statement}"}
+    return {"Result":statement}
 
 @app.post("/post/signup/account")
 def post_email(data:SigningUp):
-    information=postingEmail(data.email,data.password,data.username,data.name)
-    return {"Result":information}
+    global token
+    if token==data.TOKEN:
+        information=postingEmail(data.email,data.password,data.username,data.name)
+        token=token.replace(token,tokenGenerator.generate())
+        return {"Result":information}
+    else:
+        return {"Wrong Token":"Not valid!"}
+
+@app.get("/get/token/{email}")
+def tokenAccess(email:str):
+    global token
+    information=lookForsignup(email)
+    if information is True:
+        return {"You can't access":"Refuses"}
+    else:
+        return {"TOKEN":token}
 
 #soon...it will be completed soon
