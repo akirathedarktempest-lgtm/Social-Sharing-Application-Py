8c66c94c8cc5c66e893024088b1c1e4d2afbdb7d
Yu-zu-i chan! <akirathedarktempest@gmail.com>
2026-08-03 13:07:37 +0530

    Add user signup request to testing.py
    POST method added

--- API/testing.py
+++ API/testing.py
@@ -11,4 +11,7 @@ data=data.content
 data=data.decode("utf-8")
 data=json.loads(data)
 print(data)
+information={"email":"gmail@gmail.com","password":"password","username":"username","name":"name","TOKEN":"token"}
+response=requests.post("link/of/the/api/post/signup/account",json=information)
+print(response.status_code)
 #this literally cooked me...but done
