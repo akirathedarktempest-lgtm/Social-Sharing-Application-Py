e7b9581e6dc6e311ee43a78730ee0696b1b96b47
Yu-zu-i chan! <akirathedarktempest@gmail.com>
2026-08-05 11:59:41 +0530

    Update API endpoints and request data handling

--- API/testing.py
+++ API/testing.py
@@ -1,17 +1,17 @@
 import requests
 import json
 
-data=requests.get("link/of/the/api")
+data=requests.get("http://127.0.0.1:8000/get/bool/EMAIL")
 data=data.content
 data=data.decode("utf-8")
 data=json.loads(data)
 print(data)
-data=requests.get("link/of/the/api")
+data=requests.get("http://127.0.0.1:8000/get/token/EMAIL")
 data=data.content
 data=data.decode("utf-8")
 data=json.loads(data)
 print(data)
-information={"email":"gmail@gmail.com","password":"password","username":"username","name":"name","TOKEN":"token"}
-response=requests.post("link/of/the/api/post/signup/account",json=information)
+information={"email":"EMAIL","password":"PASSWORD","username":"USERNAME","name":"NAME","TOKEN":data["TOKEN"]}
+response=requests.post("http://127.0.0.1:8000/post/signup/account",json=information)
+print(response.content)#i didn't know that the dictionary will also be GET by POST, but it actually works
 print(response.status_code)
-#this literally cooked me...but done
